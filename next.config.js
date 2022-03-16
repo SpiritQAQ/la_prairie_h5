78c1761468eb1acec21606becdab13a7f4387ae2
Zegel <spiritqaq@gmail.com>
2022-03-16 22:13:01 +0800

    chore: config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+const PASSWORD = process.env.MONGODB_PASSWORD
+const USERNAME = process.env.MONGODB_USERNAME
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.w3slx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  },
 }
 
 module.exports = nextConfig
